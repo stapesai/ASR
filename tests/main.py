dd6ab891b9dee9cea519edf737345931a3074771
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2024-07-04 12:18:09 +0000

    chore: Update groq test notebook location and update Python version to 3.10.14

--- tests/main.py
+++ tests/main.py
@@ -3,13 +3,20 @@ from pathlib import Path
 from termcolor import colored
 from time import perf_counter
 from tabulate import tabulate
+from textwrap import fill
+import shutil
 
+# BASE_URL = "http://213.173.109.153:13417"
+# BASE_URL = "http://147.185.221.20:57813"
 BASE_URL = "http://192.168.0.253:8000"
 
 def main():
     table_data = []
+    terminal_width = shutil.get_terminal_size().columns
     
     for audio_file_path in Path("samples").glob("*"):
+        file_size_kb = audio_file_path.stat().st_size / 1024
+        file_name_with_size = f"{audio_file_path.name} ({file_size_kb:.2f} KB)"
         start_time = perf_counter()
         
         with open(audio_file_path, "rb") as audio_file:
@@ -22,16 +29,20 @@ def main():
         if response.status_code == 200:
             data = response.json()
             process_time_ms = data["process_time"] * 1000
+            # transcription = fill(data['transcription'], width=terminal_width // 2)
+            transcription = data['transcription']
             
             table_data.append([
-                colored(audio_file_path.name, "cyan"),
-                colored(data['transcription'], "green"),
+                colored(file_name_with_size, "cyan"),
+                # TODO: Add a word wrap for the transcription text
+                # colors are not displayed properly when the text is wrapped
+                colored(transcription, "green"),
                 colored(f"{process_time_ms:.2f} ms", "yellow"),
                 colored(f"{total_process_time_ms:.2f} ms", "magenta")
             ])
         else:
             table_data.append([
-                colored(audio_file_path.name, "cyan"),
+                colored(file_name_with_size, "cyan"),
                 colored("Failed to transcribe", "red"),
                 "N/A",
                 "N/A"
--- utils/whisper/__init__.py
+++ utils/whisper/__init__.py
@@ -12,7 +12,7 @@ from typing import Tuple, BinaryIO
 from .utils import load_audio, pad_or_trim
 
 SAMPLE_RATE = 16000
-MODEL_PATH = "models/whisper/whisper-small"
+MODEL_PATH = "models/whisper/whisper-medium"
 
 processor = WhisperProcessor.from_pretrained(MODEL_PATH)
 
